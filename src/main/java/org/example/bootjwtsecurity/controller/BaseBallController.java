f82ee9c8fac86fe0dad3131a7d8b1830e8057f7c
songcw8 <ben990158@gmail.com>
2025-04-23 11:09:16 +0900

    swagger

---
+++ src/main/java/org/example/bootjwtsecurity/controller/BaseBallController.java
@@ -0,0 +1,15 @@
+package org.example.bootjwtsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/baseball")
+public class BaseBallController {
+
+    @GetMapping("/teams")
+    public String findAllTeams() {
+        return "All teams";
+    }
+}
