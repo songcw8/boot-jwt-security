20e3dd604a0edd7adb633b78b77ac446c3b46792
songcw8 <ben990158@gmail.com>
2025-04-23 14:49:49 +0900

    Filter setting

---
+++ src/main/java/org/example/bootjwtsecurity/auth/JwtAuthenticationFilter.java
@@ -0,0 +1,17 @@
+package org.example.bootjwtsecurity.auth;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
+
+    }
+}
