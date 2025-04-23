267151ec9217b8954629400759e1085454b19fff
songcw8 <ben990158@gmail.com>
2025-04-23 15:12:07 +0900

    Filter Finish

--- src/main/java/org/example/bootjwtsecurity/auth/JwtAuthenticationFilter.java
+++ src/main/java/org/example/bootjwtsecurity/auth/JwtAuthenticationFilter.java
@@ -4,14 +4,28 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest req, HttpServletResponse resp, FilterChain chain) throws ServletException, IOException {
-
+        String header = req.getHeader("Authorization");
+        if(header != null || !header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            if (jwtProvider.validateToken(token)) {
+                Authentication auth = jwtProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+        chain.doFilter(req, resp);
     }
 }
