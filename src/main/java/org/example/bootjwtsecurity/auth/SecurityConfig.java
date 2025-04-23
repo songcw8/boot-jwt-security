8b774f3391d67b179ad1b54eb10750b5d1c7103f
songcw8 <ben990158@gmail.com>
2025-04-23 15:28:02 +0900

    Security Config

--- src/main/java/org/example/bootjwtsecurity/auth/SecurityConfig.java
+++ src/main/java/org/example/bootjwtsecurity/auth/SecurityConfig.java
@@ -1,29 +1,65 @@
 package org.example.bootjwtsecurity.auth;
 
+import lombok.RequiredArgsConstructor;
+import org.example.bootjwtsecurity.service.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.CsrfDsl;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+    private final JwtProvider jwtProvider;
 
     //csrf, cors 관련 여기서 설정 가능
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                //.csrf(csrf->csrf.disable())//CSRF 비활성화
-                .csrf(csrf-> csrf.ignoringRequestMatchers("/api/**"))
-                .authorizeHttpRequests(auth-> auth
-                        .requestMatchers("/api/**", "/swagger-ui/**","/v3/api-docs/**")
+                .csrf(csrf -> csrf.disable())//CSRF 비활성화
+                //.csrf(csrf-> csrf.ignoringRequestMatchers("/api/**"))
+                .sessionManagement(
+                        sm -> sm
+                                .sessionCreationPolicy(
+                                        SessionCreationPolicy.STATELESS
+                                )
+                )
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**", "/swagger-ui/**", "/v3/api-docs/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
                 )
-                .formLogin(Customizer.withDefaults())
-                .logout(Customizer.withDefaults());
+                .authenticationProvider(daoAuthenticationProvider())
+                .addFilterBefore(jwtAuthenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class);
+                //.formLogin(Customizer.withDefaults())
+                //.logout(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder(); //
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter(jwtProvider);
     }
 }
